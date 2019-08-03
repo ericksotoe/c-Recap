e3a3a7239721c47537f06e26017ca3fec007731c
eehuerta@ucsd.edu <eehuerta@ucsd.edu>
2019-08-03 01:33:56 -0700

    added output tester

---
+++ output.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  /* code */
+  cout << "starting program..." << flush;
+  cout << "this is some text." << endl;
+  cout << "banana. " << "apple. " << "orange " << endl;
+  cout << "this is some more text." << endl;
+  return 0;
+}
