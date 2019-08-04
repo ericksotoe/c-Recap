5aa1031503fbfcd0086e086e56fc2b05c6a5fab4
eehuerta@ucsd.edu <eehuerta@ucsd.edu>
2019-08-03 20:01:27 -0700

    string examples

---
+++ string.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  /* code */
+
+  string value1 = "Hello";
+  string value2 = "Erick";
+  string value3 = value1 + value2;
+
+  cout << value1 << endl;
+  cout << value2 << endl;
+  cout << value3 << endl;
+
+  return 0;
+}
