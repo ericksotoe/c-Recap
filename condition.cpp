af13b88e6513e7aa74e682b1c37d305bcbd25f75
eehuerta@ucsd.edu <eehuerta@ucsd.edu>
2019-08-04 18:20:40 -0700

    example

---
+++ condition.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  int value1 = 7;
+  int value2 = 4;
+
+  if (value1 > 8) {
+    cout << "Condition 1: true" << endl;
+  } else {
+    cout << "Condition 1: false" << endl;
+  }
+
+  return 0;
+}
