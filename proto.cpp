a4e08f14b0610cb34c3adf84e2f7aae1a34f73e1
eehuerta@ucsd.edu <eehuerta@ucsd.edu>
2019-08-12 14:48:48 -0700

    added h file

---
+++ proto.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "utils.h"
+using namespace std;
+
+// prototype 
+// void doSomething();
+
+void doSomething() {
+  cout << "Hello" << endl;
+}
+
+int main() {
+  doSomething();
+  doSomething();
+}
