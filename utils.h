8ac1cf45bf3523f1132dbbfcd0fb97d47a76db20
eehuerta@ucsd.edu <eehuerta@ucsd.edu>
2019-08-15 20:24:02 -0700

    added h file

--- utils.h
+++ utils.h
@@ -1,6 +1,8 @@
-#ifndef UTILS_H_
-#define UTILS_H_
+// these two lines are gates that make sure we only include this file once and not many times
+#ifndef UTILS_H_ // if this file is not defined
+#define UTILS_H_ // define it
 
 void doSomething();
 
+// end of the if statement
 #endif /* UTILS_H_ */
