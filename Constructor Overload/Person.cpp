d7301312768eb3b523622169b2c393a9dfd3cd7a
eehuerta@ucsd.edu <eehuerta@ucsd.edu>
2019-08-18 23:06:53 -0700

    added constructor overloading and inline implementation example

---
+++ Constructor Overload/Person.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <sstream>
+#include "Person.h"
+
+using namespace std;
+
+Person::Person() {
+  name = "undefined";
+  age = 0;
+}
+
+Person::Person(string newName, int newAge) {
+  name = newName;
+  age = newAge;
+}
+
+string Person::toString() {
+  stringstream ss;
+  ss << "Name: ";
+  ss << name;
+  ss << "; age: ";
+  ss << age;
+
+  return ss.str();
+}
